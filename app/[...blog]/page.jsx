b767f00cc403f403718f59635866422707ad5e43
muhammeddilmacc <muhammeddilmacc@gmail.com>
2024-03-03 16:11:33 +0300

    first commit

---
+++ app/[...blog]/page.jsx
@@ -0,0 +1,5 @@
+import react from 'react';
+
+export default function Blog({params}){
+    return <div>blog sayfası: {params.blog}</div>
+}
---
+++ app/hakkimizda/page.jsx
@@ -0,0 +1,4 @@
+import react from 'react';
+export default function Hakkimizda(){
+    return <div>Hakkimizda sayfasi</div>
+}
